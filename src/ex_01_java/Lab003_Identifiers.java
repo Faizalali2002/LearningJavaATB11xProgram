ce6095f7f25ef25c5a4c1fabd4c002f648c66efa
faizalali2002 <faizalali062@gmail.com>
2025-03-27 11:19:11 +0530

    27-March

--- src/ex_01_java/Lab003_Identifiers.java
+++ src/ex_01_java/Lab003_Identifiers.java
@@ -4,5 +4,17 @@ public class Lab003_Identifiers {
     public static void main(String[] args) {
         System.out.println("Identifiers");
 
+        /*
+        Identifiers:
+	• names those will be used to identify the programming elements like classes, methods, variables, etc uniquely
+
+	Rules to follow when you define an Identifier:
+	1. Identifier can contain Alphabets, Digits, and two special symbol i.e. Dollar ($), _,.
+	2. Underscore (_).(Only underscore is not allowed)
+	3. First character of an identifier must be an Alphabet or Dollar ($) or Underscore (_).
+	4. Space is not valid.
+Keywords or Reserved words can't be used as Identifiers.
+         */
+
     }
 }
