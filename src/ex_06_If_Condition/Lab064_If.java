043e392274fd039f2b3c0a691f2e6a21ea08bd20
faizalali2002 <faizalali062@gmail.com>
2025-04-07 13:42:31 +0530

    7-April

---
+++ src/ex_06_If_Condition/Lab064_If.java
@@ -0,0 +1,14 @@
+package ex_06_If_Condition;
+
+public class Lab064_If
+{
+    public static void main(String[] args) {
+        int a = 10;
+        if(a == 4){
+            System.out.println("Heyyyyyyy");
+        }
+        else{
+            System.out.println("Hellooooooo");
+        }
+    }
+}
