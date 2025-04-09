feaf7a2f763e0d48ff6699118aef2cfcd7366cf3
faizalali2002 <faizalali062@gmail.com>
2025-04-09 10:45:48 +0530

    9-April

---
+++ src/ex_08_For_Loop/Lab070_for_loop_no_condition.java
@@ -0,0 +1,9 @@
+package ex_08_For_Loop;
+
+public class Lab070_for_loop_no_condition {
+    public static void main(String[] args) {
+        for(int i = 0;  ;i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/ex_08_For_Loop/Lab071_for_loop.java
@@ -0,0 +1,15 @@
+package ex_08_For_Loop;
+
+public class Lab071_for_loop {
+    public static void main(String[] args) {
+        for (int faizal = 0; faizal < 18; faizal++) {
+            if(faizal > 15){
+                System.out.println("Gift from dad");
+            }
+            else {
+                System.out.println("no gift ");
+            }
+
+        }
+    }
+}
